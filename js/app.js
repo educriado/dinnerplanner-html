478218ef7c792783b19af62a8359c67785a96fd8
educriado <ecriado95@gmail.com>
2017-02-15 18:56:34 +0100

    Instantiate controllers in app.js.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
-$(function() {
-	//We instantiate our model
-	var model = new DinnerModel();
-	//And create the needed controllers and views
-	//var homeView = new HomeView($("#homeView"), model);
+$(function () {
+
+    // We instantiate our model
+    var model = new DinnerModel();
+
+    // Create the needed views
     var overviewContainer = document.getElementById("overviewView");
     var overviewView = new OverviewView(overviewContainer, model);
     var preparationContainer = document.getElementById("preparationView");
     var preparationView = new PreparationView(preparationContainer, model);
+
+    // Create the needed controllers
+    var overviewController = new OverviewController(overviewView, model);
+    var preparationController = new PreparationController(preparationView, model);
 });
