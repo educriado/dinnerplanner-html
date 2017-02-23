9791af615c4d909c1c11070fa02986999b186f79
educriado <ecriado95@gmail.com>
2017-02-23 01:44:03 +0100

    Commented useless code.

--- js/controller/leftMenuController.js
+++ js/controller/leftMenuController.js
@@ -1,42 +1,6 @@
 var LeftMenuController = function(view, model, overallController) {
-
-    this.getdish = function() {
-        var result = [];
-        var price = [];
-        var name = [];
-        var id = [];
-        var fullMenu = model.getFullMenu();
-
-        for (i = 0; i < fullMenu.length; i++) {
-            id.push(fullMenu[i].id);
-            price.push(model.getTotalPrice(fullMenu[i].id));
-            name.push(fullMenu[i].name);
-        }
-        result.push(id);
-        result.push(name);
-        result.push(price);
-        return result;
-    };
-
-    this.getTotalMenuPrice = function() {
-        var totalPrice1 = model.getTotalMenuPrice();
-        return totalPrice1;
-    };
-
-    this.removeDishFromMenu = function(id) {
-        model.removeDishFromMenu(id);
-    };
-
-    // $(document).on('change', '#inputNumberOfPeople', function() {
-    //     var numberOfGuest = this.value;;
-    //     if (numberOfGuest < 4) {
-    //         alert("Number of guests cannot be less than 4.");
-    //         $("#inputNumberOfPeople").val(4);
-    //     } else {
-    //         model.setNumberOfGuests(numberOfGuest);
-    //     }
-    // });
-
+    "use strict";
+    
     view.inputNumberOfPeople.change(function() {
         var numberOfGuest = inputNumberOfPeople.value;
         if (numberOfGuest < 4) {
@@ -50,4 +14,44 @@ var LeftMenuController = function(view, model, overallController) {
     view.confirmDinnerButton.click(function() {
         overallController.showDinnerOverview();
     });
-}
+
+
+
+
+    // this.getdish = function() {
+    //     var result = [];
+    //     var price = [];
+    //     var name = [];
+    //     var id = [];
+    //     var fullMenu = model.getFullMenu();
+
+    //     for (i = 0; i < fullMenu.length; i++) {
+    //         id.push(fullMenu[i].id);
+    //         price.push(model.getTotalPrice(fullMenu[i].id));
+    //         name.push(fullMenu[i].name);
+    //     }
+    //     result.push(id);
+    //     result.push(name);
+    //     result.push(price);
+    //     return result;
+    // };
+
+    // this.getTotalMenuPrice = function() {
+    //     var totalPrice1 = model.getTotalMenuPrice();
+    //     return totalPrice1;
+    // };
+
+    // this.removeDishFromMenu = function(id) {
+    //     model.removeDishFromMenu(id);
+    // };
+
+    // $(document).on('change', '#inputNumberOfPeople', function() {
+    //     var numberOfGuest = this.value;;
+    //     if (numberOfGuest < 4) {
+    //         alert("Number of guests cannot be less than 4.");
+    //         $("#inputNumberOfPeople").val(4);
+    //     } else {
+    //         model.setNumberOfGuests(numberOfGuest);
+    //     }
+    // });
+};
