c61c2cd7983e84f526c4e0206e9395eeff79fc7a
educriado <ecriado95@gmail.com>
2017-02-24 15:43:04 +0100

    This wasn't needed.

--- js/view/dishView.js
+++ js/view/dishView.js
@@ -3,7 +3,6 @@ var DishView = function(container, model) {
     "use strict";
     // Get all the relevant elements of the view (ones that show data
     // and/or ones that responed to interaction)
-    this.currentShownDishes = [];
     model.addObserver(this);
     this.dishView = container.find("#dishViewImage");
     this.searchButton = container.find("#selectDishButton");
@@ -29,7 +28,6 @@ var DishView = function(container, model) {
         this.dishView.empty();
         var key;
         for(key in dishes) {
-            currentShownDishes.push(dishes[key].id);
             this.dishView.append('<div id="' + dishes[key].id + 
                 '" style="margin-left: 128px; margin-top: 125px; margin-right: 50px; float: left;" class="img-with-text"><img src="images/'+dishes[key].image +'" alt="HTML5 Icon" style="width:128px;height:128px;"><b>'+dishes[key].name+'</b></div>');
         }
