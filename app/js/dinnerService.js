61fa9250b27c22aa38f3f5ad8f4f94f414c9d173
Eduardo Criado <ecriado95@gmail.com>
2017-03-07 00:11:39 +0100

    Dinner model API calls updated, home partial done but not working.

--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -3,34 +3,59 @@
 // dependency on any service you need. Angular will insure that the
 // service is created first time it is needed and then just reuse it
 // the next time.
-dinnerPlannerApp.factory('Dinner',function ($resource) {
-  
-  var numberOfGuest = 2;
+dinnerPlannerApp.factory('Dinner', function($resource) {
+
+    const apiKey = 'Qu9grxVNWpmshA4Kl9pTwyiJxVGUp1lKzrZjsnghQMkFkfA4LB',
+        apiRecipeSearch =
+        'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search',
+        apiRecipeInformation =
+        'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/:id/information';
+
+    var numberOfGuest = 2,
+        fullMenu = [],
+        fullMenuDetail = [],
+        currentSelectedDish = 0;
+    // Not sure if we need the previous current variables since the data is
+    // automatically binded
 
 
-  this.setNumberOfGuests = function(num) {
-    numberOfGuest = num;
-  }
+    this.setNumberOfGuests = function(num) {
+        numberOfGuest = num;
+    }
 
-  this.getNumberOfGuests = function() {
-    return numberOfGuest;
-  }
-
-
-  // TODO in Lab 5: Add your model code from previous labs
-  // feel free to remove above example code
-  // you will need to modify the model (getDish and getAllDishes) 
-  // a bit to take the advantage of Angular resource service
-  // check lab 5 instructions for details
+    this.getNumberOfGuests = function() {
+        return numberOfGuest;
+    }
 
 
+    // TODO in Lab 5: Add your model code from previous labs
+    // feel free to remove above example code
+    // you will need to modify the model (getDish and getAllDishes) 
+    // a bit to take the advantage of Angular resource service
+    // check lab 5 instructions for details
+    this.recipeSearch = $resource(apiRecipeSearch, {}, {
+        get: {
+            headers: {
+                'X-Mashape-Key': apiKey
+            }
+        }
+    });
+
+    this.recipeInfo = $resource(apiRecipeInformation, {}, {
+        get: {
+            headers: {
+                'X-Mashape-Key': apiKey
+            }
+        }
+    });
 
 
 
-  // Angular service needs to return an object that has all the
-  // methods created in it. You can consider that this is instead
-  // of calling var model = new DinnerModel() we did in the previous labs
-  // This is because Angular takes care of creating it when needed.
-  return this;
+
+    // Angular service needs to return an object that has all the
+    // methods created in it. You can consider that this is instead
+    // of calling var model = new DinnerModel() we did in the previous labs
+    // This is because Angular takes care of creating it when needed.
+    return this;
 
 });
