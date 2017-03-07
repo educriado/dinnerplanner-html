c2f9a3560bd80e3c1de404ecdf53eeac8f6ddc3b
Eduardo Criado <ecriado95@gmail.com>
2017-03-07 17:12:32 +0100

    API call works and results are displayed, have to display elements correctly.

--- TODO.md
+++ TODO.md
@@ -1,4 +1,7 @@
 # TODO
 
 - Add methods from dinnerModel.js of master branch to dinnerService.js. Some of
-them are not needed. 
+them are not needed.
+- Partials. "dinner-sidebar" partial is not being included in "search" partial,
+I don't know why.
+- CSS in assets/css/custom.css.
--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,8 +1,16 @@
 // Search controller that we use whenever we have a search inputs
 // and search results
-dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
+dinnerPlannerApp.controller('SearchCtrl', function($scope, Dinner) {
 
-  // TODO in Lab 5: you will need to implement a method that searchers for dishes
-  // including the case while the search is still running.
-
+    // TODO in Lab 5: you will need to implement a method that searchers for dishes
+    // including the case while the search is still running.
+    $scope.search = function(query, type) {
+        $scope.status = "Searching...";
+        Dinner.recipeSearch.get({ query: query, type: type }, function(data) {
+            $scope.dishes = data.results;
+            $scope.status = "Showing " + data.results.length + " results";
+        }, function(data) {
+            $scope.status = "There was an error";
+        });
+    }
 });
