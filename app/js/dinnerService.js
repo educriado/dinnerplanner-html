300c147d5c849566daf3c00544955c72797766d9
Eduardo Criado <ecriado95@gmail.com>
2017-03-23 13:55:12 +0100

    ESLint test passed for all of the code.

--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -1,77 +1,75 @@
-// Here we create an Angular service that we will use for our 
+// Here we create an Angular service that we will use for our
 // model. In your controllers (or other services) you can include the
 // dependency on any service you need. Angular will insure that the
 // service is created first time it is needed and then just reuse it
 // the next time.
-dinnerPlannerApp.factory('Dinner', function($resource) {
-
-    const apiKey = 'Qu9grxVNWpmshA4Kl9pTwyiJxVGUp1lKzrZjsnghQMkFkfA4LB',
-        apiRecipeSearch =
-        'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search',
-        apiRecipeInformation =
+angular.module('dinnerPlannerApp').factory('Dinner', function($resource) {
+  const apiKey = 'Qu9grxVNWpmshA4Kl9pTwyiJxVGUp1lKzrZjsnghQMkFkfA4LB';
+  const apiRecipeSearch =
+        'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search';
+  const apiRecipeInformation =
         'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/:id/information';
 
-    var numberOfGuest = 4,
-        fullMenu = [];
-    
+  let numberOfGuest = 4;
+  var fullMenu = [];
+
     // Not sure if we need the previous current variables since the data is
     // automatically binded
 
 
-    this.setNumberOfGuests = function(num) {
-        numberOfGuest = num;
-    }
+  this.setNumberOfGuests = function(num) {
+    numberOfGuest = num;
+  };
 
-    this.getNumberOfGuests = function() {
-        return numberOfGuest;
-    }
+  this.getNumberOfGuests = function() {
+    return numberOfGuest;
+  };
 
 
     // TODO in Lab 5: Add your model code from previous labs
     // feel free to remove above example code
-    // you will need to modify the model (getDish and getAllDishes) 
+    // you will need to modify the model (getDish and getAllDishes)
     // a bit to take the advantage of Angular resource service
     // check lab 5 instructions for details
-    this.recipeSearch = $resource(apiRecipeSearch, {}, {
-        get: {
-            headers: {
-                'X-Mashape-Key': apiKey
-            }
-        }
-    });
+  this.recipeSearch = $resource(apiRecipeSearch, {}, {
+    get: {
+      headers: {
+        'X-Mashape-Key': apiKey,
+      },
+    },
+  });
 
-    this.recipeInfo = $resource(apiRecipeInformation, {}, {
-        get: {
-            headers: {
-                'X-Mashape-Key': apiKey
-            }
-        }
-    });
+  this.recipeInfo = $resource(apiRecipeInformation, {}, {
+    get: {
+      headers: {
+        'X-Mashape-Key': apiKey,
+      },
+    },
+  });
 
-    this.getTotalMenuPrice = function() {
-        
-    };
+  this.getTotalMenuPrice = function() {
 
-    this.addDishToMenu = function(dish) {
-        fullMenu.push(dish);
-    };
+  };
 
-    this.removeDishFromMenu = function() {
-        //TODO Lab 2
-        var index = this.fullMenu.indexOf(id);
-        this.fullMenu.splice(index, 1);
-        this.notifyObservers();
-    };
+  this.addDishToMenu = function(dish) {
+    fullMenu.push(dish);
+  };
 
-    this.getTotalMenu = function() {
-        return fullMenu;
-    }
+  this.removeDishFromMenu = function(id) {
+        // TODO Lab 2
+    const index = this.fullMenu.indexOf(id);
+    this.fullMenu.splice(index, 1);
+    this.notifyObservers();
+  };
+
+  this.getTotalMenu = function() {
+    return fullMenu;
+  };
 
 
     // Angular service needs to return an object that has all the
     // methods created in it. You can consider that this is instead
     // of calling var model = new DinnerModel() we did in the previous labs
     // This is because Angular takes care of creating it when needed.
-    return this;
-
+  return this;
 });
